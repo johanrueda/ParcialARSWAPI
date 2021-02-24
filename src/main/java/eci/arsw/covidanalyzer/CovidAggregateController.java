7b934d9e80ee4011de69de3beba34fa89924d34c
johanrueda <60307772+johanrueda@users.noreply.github.com>
2021-02-24 09:23:39 -0500

    Add files via upload

---
+++ src/main/java/eci/arsw/covidanalyzer/CovidAggregateController.java
@@ -0,0 +1,48 @@
+package eci.arsw.covidanalyzer;
+
+import eci.arsw.covidanalyzer.model.Result;
+import eci.arsw.covidanalyzer.model.ResultType;
+import eci.arsw.covidanalyzer.service.ICovidAggregateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/covid/result")
+public class CovidAggregateController {
+    @Autowired
+    @Qualifier("ICovidAggregateServiceStub")
+    ICovidAggregateService covidAggregateService;
+
+    //TODO: Implemente todos los metodos POST que hacen falta.
+
+    @RequestMapping(value = "/covid/result/true-positive", method = RequestMethod.POST)
+    public ResponseEntity addTruePositiveResult(Result result) {
+        //TODO
+        covidAggregateService.aggregateResult(result, ResultType.TRUE_POSITIVE);
+        return null;
+    }
+
+    //TODO: Implemente todos los metodos GET que hacen falta.
+
+    @RequestMapping(value = "/covid/result/true-positive", method = RequestMethod.GET)
+    public ResponseEntity getTruePositiveResult() {
+        //TODO
+        //covidAggregateService.getResult(ResultType.TRUE_POSITIVE);
+        return ResponseEntity.ok("Hello World");
+    }
+
+
+    //TODO: Implemente el método.
+
+    @RequestMapping(value = "/covid/result/persona/{id}", method = RequestMethod.PUT)
+    public ResponseEntity savePersonaWithMultipleTests() {
+        //TODO
+        covidAggregateService.getResult(ResultType.TRUE_POSITIVE);
+        return null;
+    }
+    
+}
